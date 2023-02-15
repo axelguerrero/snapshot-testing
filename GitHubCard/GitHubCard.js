d7806c267bf10165d912142526c287601c816fd0
axelguerrero <guerreroaxel230@yahoo.com>
2023-02-15 16:16:01 -0500

    finished

---
+++ GitHubCard/GitHubCard.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Card from "react-bootstrap/Card"
+
+function GitHubCard() {
+  return (
+      <div className="card">
+        <Card style={{ width: "18rem"}}>
+        <Card.Body>
+            <Card.Title>axelguerrero</Card.Title>
+            <Card.Text>
+             I just be Chilling
+            </Card.Text>
+        </Card.Body>
+        </Card>
+      </div>
+  );
+}
+
+export default GitHubCard
